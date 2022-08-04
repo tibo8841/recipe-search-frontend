5a5190fb26d32cf9382779ffc65fc9bef6b8a847
tibo8841 <thibaut.hucker@gmail.com>
2022-08-04 15:54:49 +0100

    setting up components and routing

--- recipe-search-react-app/src/App.js
+++ recipe-search-react-app/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Search</h1>
+    <div>
+      <h1>recipe search</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
